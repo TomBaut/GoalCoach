d6350126bffc8c12d90f7225d81005b652d4e92f
Toom <contact@tombaut.com>
2018-12-24 14:49:49 +0100

    2

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyAQOqQGh4XpAI7_3zkNZ2510zj-WhxSb5g",
+    authDomain: "goalcoach-ce97f.firebaseapp.com",
+    databaseURL: "https://goalcoach-ce97f.firebaseio.com",
+    projectId: "goalcoach-ce97f",
+    storageBucket: "goalcoach-ce97f.appspot.com",
+    messagingSenderId: "650288320187"
+};
