415484010ccde018bc99e3fc829f545815048844
Toom <contact@tombaut.com>
2018-12-24 15:26:48 +0100

    3

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
-var config = {
+import * as firebase from 'firebase';
+
+const config = {
     apiKey: "AIzaSyAQOqQGh4XpAI7_3zkNZ2510zj-WhxSb5g",
     authDomain: "goalcoach-ce97f.firebaseapp.com",
     databaseURL: "https://goalcoach-ce97f.firebaseio.com",
@@ -6,3 +8,5 @@ var config = {
     storageBucket: "goalcoach-ce97f.appspot.com",
     messagingSenderId: "650288320187"
 };
+
+export const firebaseApp = firebase.initializeApp(config);
