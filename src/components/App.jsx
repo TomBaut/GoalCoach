51de41126fab61006fcbd056b2f6498a7abe0b2f
Toom <contact@tombaut.com>
2018-12-25 14:45:39 +0100

    3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {firebaseApp} from "../firebase";
+import AddGoal from './AddGoal';
 
 class App extends Component {
     signOut(){
@@ -8,7 +9,12 @@ class App extends Component {
     }
     render(){
         return(
-            <div>App
+            <div>
+                <div>
+                    <h3>Goals</h3>
+                    <AddGoal/>
+                    <div>Goal List</div>
+                </div>
             <button className="btn btn-danger" onClick={() => this.signOut()}>Sign Out</button>
             </div>
         )
